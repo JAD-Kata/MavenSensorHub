ed6cb1c79af0eb9d1641c8eeadd728959d80aa0c
Jean-Aymeric <jeanaymeric@gmail.com>
2024-11-13 22:37:43 +0100

    Initial commit

---
+++ data-management/src/test/java/com/jad/datamanagement/DataManagementTest.java
@@ -0,0 +1,51 @@
+package com.jad.datamanagement;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class DataManagementTest {
+    private static final String DEPENDENCY_TREE_FILE = "target/dependency-tree.txt";
+    private static final String DEPENDENCY_PREFIX = "com.jad:";
+    private static String DEPENDENCY_TREE;
+
+    @BeforeAll
+    static void readDependencyTree() throws IOException {
+        DataManagementTest.DEPENDENCY_TREE = new String(
+                Files.readAllBytes(Paths.get(DataManagementTest.DEPENDENCY_TREE_FILE)));
+    }
+
+    @Test
+    void testMainApplicationDependency() {
+        assertFalse(
+                DataManagementTest.DEPENDENCY_TREE.contains(DataManagementTest.DEPENDENCY_PREFIX + "main-application"),
+                "Dependency 'main-application' should not be present");
+    }
+
+    @Test
+    void testReportGenerationDependency() {
+        assertFalse(
+                DataManagementTest.DEPENDENCY_TREE.contains(DataManagementTest.DEPENDENCY_PREFIX + "report-generation"),
+                "Dependency 'report-generation' should not be present");
+    }
+
+    @Test
+    void testSensorDataCollectionDependency() {
+        assertFalse(
+                DataManagementTest.DEPENDENCY_TREE.contains(
+                        DataManagementTest.DEPENDENCY_PREFIX + "sensor-data-collection"),
+                "Dependency 'sensor-data-collection' should not be present");
+    }
+
+    @Test
+    void testUserInterfaceDependency() {
+        assertFalse(
+                DataManagementTest.DEPENDENCY_TREE.contains(DataManagementTest.DEPENDENCY_PREFIX + "user-interface"),
+                "Dependency 'user-interface' should not be present");
+    }
+}
